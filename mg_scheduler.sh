997921149b90ecbac14b1a10f582c0ef6295de1a
Andrei Zaika <anzaika@gmail.com>
2016-01-13 20:14:10 +0300

    extend md_scheduler

--- mg_scheduler.sh
+++ mg_scheduler.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+docker pull anzaika/selectoscope
 cd /home/anzaika/projects/selectoscope
-~/bin/docker-compose -f mg_worker_production.yml up -d
+~/bin/docker-compose -f mg_worker_production.yml build sidekiq
+~/bin/docker-compose -f mg_worker_production.yml up
