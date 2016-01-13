8a7341985bf1dcadf23eb16bdcf1826c8bab3e15
Andrei Zaika <anzaika@gmail.com>
2016-01-13 20:34:49 +0300

    add sidekiq logging in production and remove obsolete code from mg_scheduler

--- mg_scheduler.sh
+++ mg_scheduler.sh
@@ -2,5 +2,4 @@
 
 docker pull anzaika/selectoscope
 cd /home/anzaika/projects/selectoscope
-~/bin/docker-compose -f mg_worker_production.yml build sidekiq
 ~/bin/docker-compose -f mg_worker_production.yml up
