f6fac8f20903071bce568ecd81ba00cef91757d6
Andrei Zaika <anzaika@gmail.com>
2016-03-06 23:29:48 +0300

    DRY a little

--- spec/models/wrap/mafft_spec.rb
+++ spec/models/wrap/mafft_spec.rb
@@ -2,21 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Wrap::Mafft do
   describe "#execute" do
+    let(:ff) { Fabricate(:simple_fasta_file) }
+    let(:m)  { Wrap::Mafft.new(ff) }
+
     it "creates a FastaFile record after it runs" do
-      ff = Fabricate(:simple_fasta_file)
-      m = Wrap::Mafft.new(ff)
+      ff
       expect{m.execute}.to change{FastaFile.count}.by(1)
     end
 
     it "creates a RunReport record after it runs" do
-      ff = Fabricate(:simple_fasta_file)
-      m = Wrap::Mafft.new(ff)
       expect{m.execute}.to change{RunReport.count}.by(1)
     end
 
     it "creates a RunReport with successful==true record after it runs" do
-      ff = Fabricate(:simple_fasta_file)
-      m = Wrap::Mafft.new(ff)
       m.execute
       expect(m.run_report.successful).to be_equal(true)
     end
