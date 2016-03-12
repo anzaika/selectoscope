973c8d4e0d06cb59d16016e0f94c147843271a2c
Andrei Zaika <anzaika@gmail.com>
2016-03-13 00:04:43 +0300

    add more info to run_report show view

--- app/admin/run_report.rb
+++ app/admin/run_report.rb
@@ -15,9 +15,14 @@ ActiveAdmin.register RunReport do
       column span: 1 do
         panel "All" do
           attributes_table_for report do
+            row :program
+            row :successful do |resource|
+              resource.successful ? status_tag("Yes", :yellow) : status_tag("No", :red)
+            end
             row :params
             row :stdout
             row :stderr
+            row :directory_snapshot
           end
         end
       end
