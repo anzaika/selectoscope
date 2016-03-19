2ab1b98ca14e1d77303e1e9ee968788213d1fd27
Andrei Zaika <anzaika@gmail.com>
2016-03-19 23:09:34 +0300

    make run_report textifileable

--- app/models/run_report.rb
+++ app/models/run_report.rb
@@ -1,3 +1,14 @@
 class RunReport < ActiveRecord::Base
   has_many :runnable_run_report_associations, dependent: :destroy
+  has_many :text_files, as: :textifilable
+
+  def stdout
+    f = text_files.where(meta: 'stdout').first
+    f ? File.open(f.path).read : nil
+  end
+
+  def stderr
+    f = text_files.where(meta: 'stderr').first
+    f ? File.open(f.path).read : nil
+  end
 end
