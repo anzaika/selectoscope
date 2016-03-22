265166048b8cc2f18d827c63cfaef5d9bf595bf5
Andrei Zaika <anzaika@gmail.com>
2016-03-22 16:00:15 +0300

    fix run_report stdout&stderr display

--- app/models/run_report.rb
+++ app/models/run_report.rb
@@ -4,11 +4,11 @@ class RunReport < ActiveRecord::Base
 
   def stdout
     f = text_files.where(meta: 'stdout').first
-    (f ? File.open(f.file.path).read : nil).split("\n").join("</br>").html_safe
+    f ? File.open(f.file.path).read.split("\n").join("</br>").html_safe : nil
   end
 
   def stderr
     f = text_files.where(meta: 'stderr').first
-    f ? File.open(f.file.path).read : nil
+    f ? File.open(f.file.path).read.split("\n").join("</br>").html_safe : nil
   end
 end
