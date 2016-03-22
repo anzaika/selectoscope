432a67109ca6ae353314d016925129fd2529e373
Andrei Zaika <anzaika@gmail.com>
2016-03-22 17:21:04 +0300

    adapt fast job for new wrapper

--- app/jobs/fast_for_group_job.rb
+++ app/jobs/fast_for_group_job.rb
@@ -7,24 +7,9 @@ class FastForGroupJob
                   backtrace: true
 
   def perform(group_id)
-    group = Group.find(group_id).decorate
-    return nil unless group.codeml_result
-
-    alignment = group.gblocks_align
-    codeml_result = group.codeml_result
-    spec =
-      Wrap::Fast::Spec
-        .new(
-          molphy: alignment.molphy,
-          w0: codeml_result.w0,
-          w2: codeml_result.w1,
-          k: codeml_result.k,
-          p0: codeml_result.p0,
-          p1: codeml_result.p1,
-          tree: codeml_result.tree
-        )
-    report = Wrap::Fast::Run.new(spec).run
-    fast_result = FastResult.create(report.to_h)
-    group.fast_result = fast_result
+    run = Wrap::Fast::Run.new(group_id)
+    run.execute
+    report = Wrap::Fast::Report.new(run)
+    report.save
   end
 end
