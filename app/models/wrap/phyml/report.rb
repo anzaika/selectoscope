fa6f3d75ca2a0df3720d065c95c3180997e0327e
Andrei Zaika <anzaika@gmail.com>
2016-03-22 14:00:16 +0300

    remove garbage characters from newick characters when reading from file

--- app/models/wrap/phyml/report.rb
+++ app/models/wrap/phyml/report.rb
@@ -2,7 +2,7 @@ module Wrap
 class Phyml::Report < Wrap::Report
 
   def save_output
-    newick = File.open(@run.path_to_output).read
+    newick = File.open(@run.path_to_output).read.chomp.squish
     newick_decoded = Identifier.decode_string(@g.id, newick)
     tree = Tree.create(newick: newick_decoded)
 
