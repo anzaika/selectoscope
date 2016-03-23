425e5bf2324aeb29ec38a6541965fa7dd764cc2a
Andrei Zaika <anzaika@gmail.com>
2016-03-23 14:17:35 +0300

    fix abilities for regular users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,12 @@ class Ability
     if user.admin?
       can :manage, :all
     elsif user.user?
+      can %i(read update), User, id: user.id
+
       can :create, [Batch, Group]
       can :manage, [Batch, Group], user_id: user.id
 
-      can :read, [Alignment, CodemlResult, FastResult]
-
-      can %i(read update), User, id: user.id
-      can :read, RunReport, user_id: user.id
+      can :read, [Alignment, CodemlResult, FastResult, Tree, RunReport], group: {user_id: user.id}
     end
   end
 end
