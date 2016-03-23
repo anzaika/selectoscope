8d9bf2b1bd1de8463d88a03b797a9dc9bfc40726
Andrei Zaika <anzaika@gmail.com>
2016-03-23 21:23:35 +0300

    add progress tracking to full_stack_job

--- app/jobs/run_full_stack_job.rb
+++ app/jobs/run_full_stack_job.rb
@@ -1,5 +1,6 @@
 class RunFullStackJob
   include Sidekiq::Worker
+  include Sidekiq::Status::Worker
   sidekiq_options queue: :control,
                   retry: false,
                   timeout: 600.minutes,
@@ -9,12 +10,16 @@ class RunFullStackJob
     jid = AlignmentForGroupJob.perform_async(group_id)
     job_status(interval: 2, jid: jid) &&
       jid = GblocksForGroupJob.perform_async(group_id)
+    at(20, 'Alignment complete')
     job_status(interval: 5, jid: jid) &&
       jid = PhymlForGroupJob.perform_async(group_id)
+    at(40, 'Gblocks complete')
     job_status(interval: 10, jid: jid) &&
       jid = CodemlForGroupJob.perform_async(group_id)
+    at(60, 'PhyML complete')
     job_status(interval: 10, jid: jid) &&
       jid = FastForGroupJob.perform_async(group_id)
+    at(80, 'CodeML complete')
   end
 
   def job_status(interval:, jid:)
