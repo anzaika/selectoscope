ab0288d8c0d24aa4d3f7647c3658969962b1f3b0
Andrei Zaika <anzaika@gmail.com>
2016-03-23 21:29:56 +0300

    add new type of fasta extension *.fas

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,6 +2,7 @@ Paperclip.options[:content_type_mappings] = {
   fasta: "text/plain",
   fst: "text/plain",
   fa: "text/plain",
+  fas: "text/plain",
   out: "text/plain",
   "fa-gb": "text/plain"
 }
