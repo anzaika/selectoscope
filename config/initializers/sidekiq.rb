42ebd3a256cf3ee7bae35184760e4cf01652cf19
Andrei Zaika <anzaika@gmail.com>
2016-03-24 06:37:25 +0300

    extend persistence time for sidekiq::status data to 100 hours

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,16 +3,16 @@ if Rails.env.production?
   Sidekiq.configure_server do |config|
     config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379/12" }
     config.server_middleware do |chain|
-      chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes
+      chain.add Sidekiq::Status::ServerMiddleware, expiration: 100.hours
     end
     config.client_middleware do |chain|
-      chain.add Sidekiq::Status::ClientMiddleware, expiration: 30.minutes
+      chain.add Sidekiq::Status::ClientMiddleware, expiration: 100.hours
     end
   end
   Sidekiq.configure_client do |config|
     config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379/12" }
     config.client_middleware do |chain|
-      chain.add Sidekiq::Status::ClientMiddleware, expiration: 30.minutes
+      chain.add Sidekiq::Status::ClientMiddleware, expiration: 100.hours
     end
   end
 
