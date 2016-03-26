3c97e53cf0db210788a1aa6587a5262bb5e72db7
Andrei Zaika <anzaika@gmail.com>
2016-03-26 17:15:36 +0300

    add run_report scopes

--- app/models/run_report.rb
+++ app/models/run_report.rb
@@ -2,6 +2,12 @@ class RunReport < ActiveRecord::Base
   belongs_to :group
   has_many :text_files, as: :textifilable, dependent: :destroy
 
+  scope :alignment, -> { where(program: Wrap::Phyml::Run::PROGRAM) }
+  scope :processed_alignment, -> { where(program: Wrap::Gblocks::Run::PROGRAM) }
+  scope :tree, -> { where(program: Wrap::Phyml::Run::PROGRAM) }
+  scope :codeml, -> { where(program: Wrap::Codeml::Run::PROGRAM) }
+  scope :fast, -> { where(program: Wrap::Fast::Run::PROGRAM) }
+
   def stdout
     f = text_files.where(meta: 'stdout').first
     f ? File.open(f.file.path).read.split("\n").join("</br>").html_safe : nil
