51149f88136da6cd4af92e3a2fc5255a45a26abf
Andrei Zaika <anzaika@gmail.com>
2016-05-03 13:59:52 +0300

    fix fasta_file_spec

--- spec/models/fasta_file_spec.rb
+++ spec/models/fasta_file_spec.rb
@@ -13,19 +13,17 @@
 #  file_updated_at             :datetime
 #
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe FastaFile, type: :model do
-  let(:ff) { Fabricate(:simple_fasta_file)}
+  let(:ff) { Fabricate(:simple_fasta_file) }
   it { is_expected.to respond_to(:file) }
   it { is_expected.to respond_to(:to_bioruby_alignment_object) }
 
-
-  describe "#to_array_of_two_element_arrays_with_desc_and_seq" do
+  describe '#to_array_of_two_element_arrays_with_desc_and_seq' do
     it "shoud return what it says =)" do
       run_result = ff.to_array_of_two_element_arrays_with_desc_and_seq
-      expect(run_result).to eq( [['seq1','atgc'],['seq2','atgt']] )
+      expect(run_result).to eq([%w(seq1 ata), %w(seq2 atg), %w(seq3 att), %w(seq4 atc)])
     end
   end
-
 end
