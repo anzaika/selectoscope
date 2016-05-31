3f547df7d946f0877c04380460324e8c33a86e0e
Andrei Zaika <anzaika@gmail.com>
2016-05-31 10:47:44 +0300

    code style

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,15 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
-require 'rspec/rails'
-require 'fabrication'
-require 'database_cleaner'
-require_relative 'support/database_cleaner'
+require "spec_helper"
+require "rspec/rails"
+require "fabrication"
+require "database_cleaner"
+require_relative "support/database_cleaner"
 
-require 'sidekiq/testing'
+require "sidekiq/testing"
 Sidekiq::Testing.inline!
 
 ActiveRecord::Migration.maintain_test_schema!
