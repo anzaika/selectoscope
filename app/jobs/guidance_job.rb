aa33a8fbbf25b8d41b87c8d85c9c1f574cc68513
Andrei Zaika <anzaika@gmail.com>
2016-06-01 22:17:18 +0300

    code style

--- app/jobs/guidance_job.rb
+++ app/jobs/guidance_job.rb
@@ -1,9 +1,9 @@
 class GuidanceJob
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
-  sidekiq_options queue: :many,
-                  retry: false,
-                  timeout: 60.minutes,
+  sidekiq_options queue:     :many,
+                  retry:     false,
+                  timeout:   60.minutes,
                   backtrace: true
 
   def perform(group_id)
