1d029a2b47b62dcd9fdaf25c5f4621d3f7d5e7fc
Andrei Zaika <anzaika@gmail.com>
2016-06-02 20:17:45 +0300

    add Fabricator for TextFile

--- spec/fabricators/text_files.rb
+++ spec/fabricators/text_files.rb
@@ -1,2 +1,11 @@
 Fabricator(:text_file) do
+  transient :content
+  textifilable { Fabricate(:tool_run_report) }
+  file do |attrs|
+    f = Tempfile.new(Faker::Lorem.word)
+    content = attrs[:content] || Faker::Lorem.paragraphs(5).join("\n")
+    f.write(content)
+    f.rewind
+    File.open(f.path)
+  end
 end
