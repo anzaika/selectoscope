a8c5d45081df91575031fbec9d54b0232a5d0b44
Andrei Zaika <anzaika@gmail.com>
2016-06-02 20:18:30 +0300

    extent tool_run_report spec

--- spec/models/tool_run_report_spec.rb
+++ spec/models/tool_run_report_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe ToolRunReport, type: :model do
       Fabricate(:tool_run_report)
     end
   end
+
+  describe "decode_all_text_files" do
+    it "shou decode all files" do
+      profile = Fabricate(:run_profile_run_report)
+      report = Fabricate(:tool_run_report, run_profile_run_report: profile)
+      ident = profile.group.identifiers.first
+      codename = ident.codename
+      name = ident.name
+      textfile = Fabricate(:text_file, textifilable: report, content: codename)
+      report.decode_all_text_files
+      expect(textfile.raw.include?(name)).to be true
+    end
+  end
 end
