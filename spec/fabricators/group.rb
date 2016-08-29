51052e98bd14f70b664c1679df22661cb4aea3ae
Andrei Zaika <anzaika@gmail.com>
2016-08-29 21:13:32 +0300

    remove obsolete group fabricators

--- spec/fabricators/group.rb
+++ spec/fabricators/group.rb
@@ -6,21 +6,6 @@ Fabricator(:group_with_simple_fasta, from: :group) do
   fasta_file { Fabricate(:simple_fasta_file) }
 end
 
-Fabricator(:group_with_complicated_fasta, from: :group) do
-  fasta_file { Fabricate(:complicated_fasta_file) }
-end
-
-Fabricator(:group_with_alignment, from: :group) do
-  fasta_file { Fabricate(:complicated_fasta_file) }
-  alignments { [Fabricate(:original_alignment_for_complicated_fasta)] }
-end
-
-Fabricator(:group_with_tree_and_alignment, from: :group) do
-  fasta_file { Fabricate(:complicated_fasta_file) }
-  alignments { [Fabricate(:original_alignment_for_complicated_fasta)] }
-  tree { Fabricate(:tree_for_aligned_complicated_fasta) }
-end
-
 Fabricator(:group_with_good_fast, from: :group) do
   alignments(count: 2)
   tree do
