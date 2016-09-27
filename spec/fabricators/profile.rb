7a7554c92706dc1099cc1bc0ad2c344ef226c68a
Andrei Zaika <anzaika@gmail.com>
2016-09-28 01:04:11 +0200

    update Profile fabricator

--- spec/fabricators/profile.rb
+++ spec/fabricators/profile.rb
@@ -1,7 +1,8 @@
 Fabricator(:profile) do
   name { Faker::Lorem.paragraph.split(" ").sample }
-  tool_for_alignment_id { Fabricate(:tool_for_alignment).id }
-  tool_for_tree_id { Fabricate(:tool_for_tree).id }
-  tool_for_selection_id { Fabricate(:tool_for_selection).id }
+  tool_for_alignment_id { Fabricate(:tool).id }
+  tool_for_filtering_id { Fabricate(:tool).id }
+  tool_for_tree_id { Fabricate(:tool).id }
+  tool_for_selection_id { Fabricate(:tool).id }
   user
 end
