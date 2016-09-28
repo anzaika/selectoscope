956f8fda9760bf5f120481d7737894d165e9d328
Andrei Zaika <anzaika@gmail.com>
2016-09-28 15:52:44 +0200

    remove obsolete route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
