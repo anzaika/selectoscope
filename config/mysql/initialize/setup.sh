226568a19e19052876728157b3823f4d4d47c6d3
Andrei Zaika <anzaika@gmail.com>
2016-09-28 11:40:56 +0200

    finalize mysql init script

--- config/mysql/initialize/setup.sh
+++ config/mysql/initialize/setup.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
-
-# Initialize MySQL database.
-# ADD this file into the container via Dockerfile.
-# Assuming you specify a VOLUME ["/var/lib/mysql"] or `-v /var/lib/mysql` on the `docker run` command…
-# Once built, do e.g. `docker run your_image /path/to/docker-mysql-initialize.sh`
-# Again, make sure MySQL is persisting data outside the container for this to have any effect.
-
 set -e
 set -x
 
-# Start the MySQL daemon in the background.
-# /usr/sbin/mysqld &
-# mysql_pid=$!
-
-# Wait for database to initialize
-sleep 10
-
+service mysql start
 until mysqladmin ping &>/dev/null; do
   echo -n "."; sleep 1
 done
-
-# Permit root login without password from outside container.
-mysql -uroot -p1234 -e "GRANT ALL ON *.* TO 'root'@'%'"
-
-# Tell the MySQL daemon to shutdown.
-mysqladmin shutdown
-
-# Wait for the MySQL daemon to exit.
-# wait $mysql_pid
+mysql -uroot -p1234 -e "GRANT ALL ON *.* TO 'root'@'%' identified by '1234'"
+service mysql stop
