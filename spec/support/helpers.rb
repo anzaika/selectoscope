61c6fc5fb57871b1b96ffe8741a4ed581e8e6c50
Andrei Zaika <anzaika@gmail.com>
2016-09-30 08:21:42 +0200

    add spec helper

---
+++ spec/support/helpers.rb
@@ -0,0 +1,17 @@
+require 'bio'
+require 'faker'
+
+module Helpers
+  def self.dna_string
+    counts = {'a'=>3,'c'=>3,'g'=>3,'t'=>3}
+    "atg" + Bio::Sequence::NA.randomize(counts)
+  end
+
+  def self.fasta
+    4.times.map{|i| ">#{i}\n#{self.dna_string}" }.join("\n")
+  end
+
+  def self.fixed_fasta
+    4.times.map{|i| ">#{i}\natgatttta" }.join("\n")
+  end
+end
